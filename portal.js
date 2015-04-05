e3b2706436b1dc00a8551a4290b8ca4089ac8866
Lance Orner <lance@orner.net>
2015-04-05 14:12:00 -0700

    Working version

---
+++ portal.js
@@ -0,0 +1,118 @@
+/* portal.js */
+
+/*
+ * Lance Orner, December 22, 2014
+ */
+
+window.onload = init;
+
+// Use when debugging to prevent lots of hits to server
+var use_local = false;
+
+function init() {
+	getWeather();
+  getStirlingWeather();
+  getStock();
+}
+
+/*
+ * Link functions
+ */
+
+
+/*
+ * Weather functions
+ */
+
+function getWeather() {
+  var url = "http://api.wunderground.com/api/b37c48462659c2fe/geolookup/conditions/forecast/q/39.64018250,-119.72023010.json";
+  if (use_local) {
+    url = "http://localhost/~lance/portal/weather.json";
+  }
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+	request.onload = function() {
+		if (request.status == 200) {
+			updateWeather(request.responseText);
+		}
+	};
+	request.send(null);
+}
+
+function getStirlingWeather() {
+  var url = "http://api.wunderground.com/api/b37c48462659c2fe/geolookup/conditions/forecast/q/39.90683934719737,-121.52741432189941.json";
+  if (use_local) {
+    url = "http://localhost/~lance/portal/weather.json";
+  }
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+	request.onload = function() {
+		if (request.status == 200) {
+			updateWeather(request.responseText);
+		}
+	};
+	request.send(null);
+
+}
+
+function updateWeather(responseText) {
+  var weatherDiv = document.getElementById("weather");
+  var weather = JSON.parse(responseText);
+
+  var location = weather['location']['city'];
+  var temp_f = weather['current_observation']['temp_f'];
+  var temp = weather['current_observation']['temperature_string'];
+  var observation = weather['current_observation']['weather'];
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "tempurature");
+  div.innerHTML = location + ": " + observation + " and " + temp;
+  weatherDiv.appendChild(div);
+}
+
+
+function getStock() {
+  var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22IGT%22%2C%22GOOG%22%2C%22MSFT%22%2C%22JCP%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+	request.onload = function() {
+		if (request.status == 200) {
+			updateStock(request.responseText);
+		}
+	};
+	request.send(null);
+}
+
+function updateStock(responseText) {
+  var stockDiv = document.getElementById("stock");
+  var stock = JSON.parse(responseText);
+
+  var quotes = stock.query.results.quote;
+  var quoteline = "";
+  for (var i = 0; i < quotes.length; ++i)
+  {
+    var quote = quotes[i];
+    var name = quote.Symbol;
+    var price = quote.LastTradePriceOnly;
+    var change = quote.Change;
+    var changePercent = quote.ChangeinPercent;
+    var quoteSpan = document.createElement("span");
+    quoteSpan.setAttribute("class", "quote");
+    quoteSpan.innerText = name + ": " + price;
+
+    var changeSpan = document.createElement("span");
+    changeSpan.setAttribute("class", "quotechange");
+    if (change>0) {
+      changeSpan.setAttribute("delta", "positive");
+    } else if (change < 0) {
+      changeSpan.setAttribute("delta", "negative");
+    }
+    changeSpan.innerText = "("+changePercent + ")";
+
+    quoteSpan.appendChild(changeSpan);
+    quoteSpan.innerHTML += "  ";
+
+    stockDiv.appendChild(quoteSpan);
+  }
+
+}
